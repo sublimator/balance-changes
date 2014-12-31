90621ce3138914911b6ace4a687d75db220318c4
Nicholas Dudfield <ndudfield@gmail.com>
2014-12-31 15:21:54 +0700

    Fixes

--- src/balance-changes.js
+++ src/balance-changes.js
@@ -5,21 +5,17 @@ var bn = require('bignumber.js');
 
 /* --------------------------------- HELPERS -------------------------------- */
 
-var ZERO = new bn(0);
-function xrp_to_drops(dropsString) { return new bn(dropsString).div(1e6); }
+function drops_to_xrp(dropsString) { return new bn(dropsString).div(1e6); }
 
 function normalize_affected(affected) {
   // ModifiedNode/CreatedNode/DeletedNode
   var affected_type = Object.keys(affected)[0];
   // Unwrap
-  affected = affected[affected_type];
-
+  var affected = affected[affected_type];
   // Create the state of the node after the tx;
-  var finals = affected.NewFields != null ?
-               affected.NewFields : affected.FinalFields;
+  var finals = affected.NewFields ? affected.NewFields : affected.FinalFields;
 
-  after_tx = extend({}, finals);
-
+  var after_tx = extend({}, finals);
   // Copy all non object fields
   for (var k in affected) {
     var o = affected[k];
@@ -31,20 +27,20 @@ function normalize_affected(affected) {
   // The LedgerIndex is a field that's only in the metadata
   // mv it to `index`
   after_tx.index = after_tx.LedgerIndex;
-  delete after_tx.LedgerIndex;
+  after_tx.LedgerIndex = (void 0);
 
-  if (affected_type == 'CreatedNode') {
-    before_tx = null;
+  if (affected_type === 'CreatedNode') {
+    var before_tx = null;
   } else {
     // The PreviousFields object is limited to fields that change, so we start
     // with the state after the tx, and then layer over the changed fields.
-    before_tx = extend({}, after_tx, affected.PreviousFields);
+    var before_tx = extend({}, after_tx, affected.PreviousFields);
   }
 
   return {after_tx: after_tx,
           before_tx: before_tx,
           index: after_tx.index,
-          is_new: affected_type == 'CreatedNode',
+          is_new: affected_type === 'CreatedNode',
           previous_fields: affected.PreviousFields || {},
           ledger_entry_type: affected.LedgerEntryType};
 }
@@ -55,61 +51,57 @@ function aci_key(account, currency, issuer) {
   return [account, currency, issuer].join(':');
 }
 
-function Tally() {
+function BalanceTally() {
   // A running tally of amounts keyed by account/currency/issuer
   this.amounts = {};
 }
 
-Tally.prototype.add = function(holder, currency, issuer, amount) {
-  if (amount.isZero()) {
-    return;
-  };
-
+BalanceTally.prototype.add = function(holder, currency, issuer, amount) {
   var key = aci_key(holder, currency, issuer);
   var existing = this.amounts[key];
 
-  if (existing == null)
+  if (!existing)
     this.amounts[key] = amount;
   else
     this.amounts[key] = existing.plus(amount);
 }
 
-Tally.prototype.subtract = function(holder, currency, issuer, amount) {
+BalanceTally.prototype.subtract = function(holder, currency, issuer, amount) {
   this.add(holder, currency, issuer, amount.neg());
 }
 
-Tally.prototype.changes_list = function(opts) {
+BalanceTally.prototype.changes_list = function(opts) {
+  var result = [];
   var self = this;
   var keys = Object.keys(this.amounts);
   if (opts && opts.sorted) {keys.sort()};
 
-  return keys.map(function(aci) {
+  keys.forEach(function(aci) {
     var balance = self.amounts[aci];
     // We don't care about non changes
-    if (balance.isZero()) {
-      return null;
+    if (!balance.isZero()) {
+      aci = aci.split(':');
+
+      result.push ({
+        account: aci[0],
+        balance_change: {
+          value: balance.toString(),
+          currency: aci[1],
+          issuer: aci[2] || ''
+        }
+      });
     };
+  });
 
-    aci = aci.split(':');
-
-    return ({
-      account: aci[0],
-      balance_change: {
-        value: balance.toString(),
-        currency: aci[1],
-        issuer: aci[2] || ''
-      }
-    });
-  })
-  .filter(function(o) {return o != null});
+  return result;
 }
 
 function tally_root_balances(tally, before_tx, after_tx) {
   function do_tally(fields, opts) {
-    if (fields.Balance != null && fields.Account != null)
+    if (fields.Balance && fields.Account)
     {
        var operation = opts && opts.negate ? 'subtract' : 'add';
-       tally[operation](fields.Account, 'XRP', '',xrp_to_drops(fields.Balance));
+       tally[operation](fields.Account, 'XRP', '',drops_to_xrp(fields.Balance));
     }
   }
   if (before_tx) do_tally(before_tx, {negate: true});
@@ -118,35 +110,35 @@ function tally_root_balances(tally, before_tx, after_tx) {
 
 function tally_line_balances(tally, before_tx, after_tx) {
   function do_tally(fields, opts) {
-    if (fields.Balance != null)
-    {
-       var balance = bn(fields.Balance.value);
-       if (balance.isZero()) {
-        // nothing to see here
-        return;
-       }
+    var balance = bn(fields.Balance.value);
+    if (balance.isZero()) {
+    // Remember for the before_tx fields we subtract amounts so for non-zero to
+    // zero, the difference is implied.
+    return;
+    }
 
-       var currency = fields.Balance.currency;
-       // The difference == After - Before.
-       var operation = opts && opts.negate ? 'subtract' : 'add';
+    var currency = fields.Balance.currency;
+    // The difference == after_tx -  before_tx
+    var operation = opts && opts.negate ? 'subtract' : 'add';
+    // DRY it up, for readability
+    function record(holder, amount, issuer) {
+      tally[operation](holder, currency, issuer, amount);
+    }
 
-       // DRY it up
-       function record(holder, amount, issuer) {
-          tally[operation](holder, currency, issuer, amount);
-       }
+    var low_account = fields.LowLimit.issuer;
+    var hi_account = fields.HighLimit.issuer;
 
-       var negatedBalance = balance.neg();
-       var lowAccount = fields.LowLimit.issuer;
-       var hiAccount = fields.HighLimit.issuer;
-
-       if (balance.isNegative()) {
-         record(lowAccount, balance, lowAccount);
-         record(hiAccount, negatedBalance, lowAccount);
-       }
-       else {
-         record(lowAccount, balance, hiAccount);
-         record(hiAccount, negatedBalance, hiAccount);
-       }
+    if (balance.isNegative()) {
+     // low account is issuing so has a negative balance of own funds
+     record(low_account, balance, low_account);
+     // conversely, the high account has a positive balance of the low
+     // accounts's currency.
+     record(hi_account, balance.neg(), low_account);
+    }
+    else {
+     // this is the alternative to the above block.
+     record(low_account, balance, hi_account);
+     record(hi_account, balance.neg(), hi_account);
     }
   }
   if (before_tx) do_tally(before_tx, {negate: true});
@@ -156,7 +148,7 @@ function tally_line_balances(tally, before_tx, after_tx) {
 /* ------------------------------- PUBLIC API ------------------------------- */
 
 exports.find_changes = function(meta) {
-  var tally = new Tally();
+  var tally = new BalanceTally();
 
   meta.AffectedNodes.forEach(function(raw_affected) {
     var affected = normalize_affected(raw_affected);
@@ -164,7 +156,7 @@ exports.find_changes = function(meta) {
     // An optimisation
     if (
       // If the Balance wasn't changed, there's nothing to see
-      affected.previous_fields.Balance == null &&
+      !affected.previous_fields.Balance &&
       // unless of course it's a new node
       !affected.is_new) {
       return;
@@ -173,9 +165,9 @@ exports.find_changes = function(meta) {
     var before_tx = affected.before_tx;
     var after_tx = affected.after_tx;
 
-    if (affected.ledger_entry_type == 'AccountRoot')
+    if (affected.ledger_entry_type === 'AccountRoot')
       tally_root_balances(tally, before_tx, after_tx);
-    else if (affected.ledger_entry_type == 'RippleState')
+    else if (affected.ledger_entry_type === 'RippleState')
       tally_line_balances(tally, before_tx, after_tx);
   });
 
